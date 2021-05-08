8edb2dc3eb3e0cbb1210b8647a277ceb6569fe42
Arijit Banerjee <arijit.banerjee@nike.com>
2021-05-08 16:13:30 -0700

    adding detailed logging

--- background.js
+++ background.js
@@ -1,5 +1,19 @@
 var tabURL = "nothing"
-var tabTitle ="n/a"
+var tabTitle = "n/a"
+
+function getLocation(href) {
+    var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+    return match && {
+        protocol: match[1],
+        host: match[2],
+        hostname: match[3],
+        port: match[4],
+        pathname: match[5],
+        search: match[6],
+        hash: match[7]
+    }
+}
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     tabURL = tab.url
     tabTitle = tab.title
@@ -7,8 +21,8 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.action == "tabCount"){
-            sendResponse({"tabURL": tabURL,"tabTitle": tabTitle});
+        if (request.action == "tabCount") {
+            sendResponse({"tabURL": tabURL, "tabTitle": tabTitle, "tabDetails": getLocation(tabURL)});
         }
     });
 
--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,8 @@
 var _LTracker = _LTracker || [];
-
+var compName =""
 
 chrome.storage.sync.get(['apiKey','compName'], function(result) {
+    compName = result.compName
     _LTracker.push({
         'logglyKey': result.apiKey,
         'sendConsoleErrors': true,
@@ -12,7 +13,9 @@ chrome.storage.sync.get(['apiKey','compName'], function(result) {
 chrome.runtime.sendMessage({action: "tabCount"}, function(response) {
     var message = {
         'url': response.tabURL,
-        'title' : response.tabTitle
+        'title' : response.tabTitle,
+        'details' : response.tabDetails,
+        'compName' : compName
     }
     _LTracker.push(message);
 });
